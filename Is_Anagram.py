ca81bd7b896f0ac2b7d928452c99beef9318a666
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-25 14:48:15 -0500

    Commit with instructions

---
+++ Is_Anagram.py
@@ -0,0 +1,6 @@
+# Function names 'is_anagram' takes two string as its parameters.
+#It returns True if the string are anagrams, and False otherwise.
+
+#E.g, is_anagram("typhoon", "opython") should return True
+# While the call is_anagram("Alice", "Bob") should return False.
+
