d390dc780abb5a0df4d72f199167678530832ffe
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-31 08:17:01 -0500

    Finished function

--- Is_Anagram.py
+++ Is_Anagram.py
@@ -4,3 +4,10 @@
 #E.g, is_anagram("typhoon", "opython") should return True
 # While the call is_anagram("Alice", "Bob") should return False.
 
+def is_anagram(a, b): 
+    auth = False if len([i for i in list(a) if i not in list(b)]) >= 1 else True
+    return auth
+
+print(is_anagram("typhoon", "opython")) #True
+print(is_anagram("typhozon", "opython")) #False
+print(is_anagram("typhhozon", "opython")) #False
